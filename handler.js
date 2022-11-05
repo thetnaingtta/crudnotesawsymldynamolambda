d48d9c76c403a53ab007e7ed7de6d9cb33b0de75
Thet Naing <thetnaing@mirandah.com>
2022-11-05 15:53:01 +0800

    adding timeout in dynamo and HTTP reuse value

--- handler.js
+++ handler.js
@@ -1,9 +1,23 @@
 "use strict";
 const DynamoDB = require("aws-sdk/clients/dynamodb");
-const documentClient = new DynamoDB.DocumentClient({ region: "us-east-1" });
+const documentClient = new DynamoDB.DocumentClient({
+  region: "us-east-1",
+  maxRetries: 3,
+  httpOptions: {
+    timeout: 5000,
+  },
+});
 const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
 
+const send = (statuscode, data) => {
+  return {
+    statuscode,
+    body: JSON.stringify(data),
+  };
+};
+
 module.exports.createNote = async (event, context, cb) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   let data = JSON.parse(event.body);
   try {
     const params = {
@@ -16,19 +30,14 @@ module.exports.createNote = async (event, context, cb) => {
       ConditionExpression: "attribute_not_exists(notesId)",
     };
     await documentClient.put(params).promise();
-    cb(null, {
-      statusCode: 201,
-      body: JSON.stringify(data),
-    });
+    cb(null, send(201, data));
   } catch (err) {
-    cb(null, {
-      statusCode: 500,
-      body: JSON.stringify(err.message),
-    });
+    cb(null, send(500, err.message));
   }
 };
 
 module.exports.updateNote = async (event, context, cb) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   let notesId = event.pathParameters.id;
   let data = JSON.parse(event.body);
 
@@ -50,19 +59,14 @@ module.exports.updateNote = async (event, context, cb) => {
       ConditionExpression: "attribute_exists(notesId)",
     };
     await documentClient.update(params).promise();
-    cb(null, {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    });
+    cb(null, send(200, data));
   } catch (err) {
-    cb(null, {
-      statusCode: 500,
-      body: JSON.stringify(err.message),
-    });
+    cb(null, send(500, err.message));
   }
 };
 
 module.exports.deleteNote = async (event, context, cb) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   let notesId = event.pathParameters.id;
   try {
     const params = {
@@ -71,32 +75,21 @@ module.exports.deleteNote = async (event, context, cb) => {
       ConditionExpression: "attribute_exists(notesId)",
     };
     await documentClient.delete(params).promise();
-    cb(null, {
-      statusCode: 200,
-      body: JSON.stringify(notesId),
-    });
+    cb(null, send(200, data));
   } catch (err) {
-    cb(null, {
-      statusCode: 500,
-      body: JSON.stringify(err.message),
-    });
+    cb(null, send(500, err.message));
   }
 };
 
 module.exports.getAllNotes = async (event, context, cb) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   try {
     const params = {
       TableName: NOTES_TABLE_NAME,
     };
     const notes = await documentClient.scan(params).promise();
-    cb(null, {
-      statusCode: 200,
-      body: JSON.stringify(notes),
-    });
+    cb(null, send(200, data));
   } catch (err) {
-    cb(null, {
-      statusCode: 500,
-      body: JSON.stringify(err.message),
-    });
+    cb(null, send(500, err.message));
   }
 };
