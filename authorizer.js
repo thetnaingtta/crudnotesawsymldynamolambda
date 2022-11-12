276d8b95505278dd9dca88e9479df95eaa5397a5
thetnaingtta <114322762+thetnaingtta@users.noreply.github.com>
2022-11-12 08:59:29 +0800

    adding cognito

--- authorizer.js
+++ authorizer.js
@@ -1,3 +1,11 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: "us-east-1_QeTO3K84Q",
+  tokenUse: "id",
+  clientId: "105k0u0jgddhq6rnjcuath78p3",
+});
+
 const generatePolicy = (principalId, effect, resource) => {
   var authRepsonse = {};
 
@@ -26,17 +34,33 @@ const generatePolicy = (principalId, effect, resource) => {
   return authRepsonse;
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   // lambda authorizer code
-  var token = event.authorizationToken; // "allow or deny"
-  switch (token) {
-    case "allow":
-      callback(null, generatePolicy("user", "Allow", event.methodArn));
-      break;
-      case "deny":
-        callback(null, generatePolicy("user", "Deny", event.methodArn));
-        break;
-        default:
-            callback("Error: Invalid Token")
+
+  var token = event.authorizationToken;
+  console.log(token);
+  // Validate the token
+
+  try {
+    const payload = await jwtVerifier.verify(token);
+    console.log(JSON.stringify(payload));
+    callback(null, generatePolicy("user", "Allow", event.methodArn));
+  } catch (err) {
+    callback("Error: Invalid Token") 
   }
+
+  // var token = event.authorizationToken; // "allow or deny"
+  // switch (token) {
+  //   case "allow":
+  //     callback(null, generatePolicy("user", "Allow", event.methodArn));
+  //     break;
+  //     case "deny":
+  //       callback(null, generatePolicy("user", "Deny", event.methodArn));
+  //       break;
+  //       default:
+  //           callback("Error: Invalid Token")
+  // }
+
+
+  // https://mynotesdemomodernsg.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=105k0u0jgddhq6rnjcuath78p3&redirect_uri=http://localhost:3000
 };
