340962b06078eec3a1c7bd9b1f80f2850a442b9f
Thet Naing <thetnaing@mirandah.com>
2022-11-23 17:45:47 +0800

    adding enviournment variables and dynamic stages

--- authorizer.js
+++ authorizer.js
@@ -1,9 +1,11 @@
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
+const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
+const COGNITO_WEB_CLIENT_ID = process.env.COGNITO_WEB_CLIENT_ID;
 
 const jwtVerifier = CognitoJwtVerifier.create({
-  userPoolId: "us-east-1_MEH2gUr1K",
+  userPoolId: COGNITO_USERPOOL_ID,
   tokenUse: "id",
-  clientId: "4fecbrka91i1r2c8cjhknh94ca",
+  clientId: COGNITO_WEB_CLIENT_ID,
 });
 
 const generatePolicy = (principalId, effect, resource) => {
